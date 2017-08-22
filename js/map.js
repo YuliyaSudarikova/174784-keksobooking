5dc9a53b9e43e3f4676db78c5521f795b3dd8999
YuliyaSudarikova <yvsudarikova@icloud.com>
2017-08-22 13:18:00 +0300

    попытки разобраться

---
+++ js/map.js
@@ -0,0 +1,39 @@
+"use strict";
+var numberArray = function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var object = function getRandom(numberArray) {
+  return similarObject [numberArray];
+}
+
+for (var i = 0; i < similarObjectOptions.length; i++) {
+  var similarObject = similarObjectOptions[i];
+}
+
+
+
+
+var similarObjectOptions = [
+  'author': {
+    'avatar': 'img/avatars/user{{xx}}.png';
+            }
+  },
+  'offer': {
+    'title': 'Большая уютная квартира, Маленькая неуютная квартира, Огромный прекрасный дворец, Маленький ужасный дворец, Красивый гостевой домик, Некрасивый негостеприимный домик, Уютное бунгало далеко от моря, Неуютное бунгало по колено в воде',
+    'price': '1000 до 1 000 000',
+    'type': 'flat, house, bungalo',
+    'rooms': roomsNumber,
+    'guests': guestsNumber,
+    'checkin': '12:00, 13:00, 14:00',
+    'checkout': '12:00, 13:00, 14:00',
+    'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    'description': '',
+    'photos': []
+  },
+  'location': {
+  'x': '.tokyo__pin-map от 300 до 900',
+  'y': '.tokyo__pin-map от 100 до 500'
+}
+]
+var similarObjectCard = document.querySelection('.tokyo');
