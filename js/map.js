7cda913fdae67649ed0d234b5212d75a14fb48fe
YuliyaSudarikova <yvsudarikova@icloud.com>
2017-08-29 16:50:02 +0300

    с начала

--- js/map.js
+++ js/map.js
@@ -1,21 +1,42 @@
-"use strict";
-//генерим случайное число
+'use strict';
+
+// генерим случайное число
 function getRandomNumber(min, max) {
-  getRandomNumber.x = 0;
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 // взять случайный объект из массива
 function randItem(arr) {
   return arr[getRandomNumber(0, arr.length)];
-};
+}
 
-// генерим неповторяющиеся строки
-function getNonRepeatingNumber() {
-  return getNonRepeatingNumber.a.pop();
-};
+// рандомные удобства
+function getRandomFeatures() {
+  var features = [];
+  var allFeatures = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+  ];
+  var featureCount = getRandomNumber(0, allFeatures.length);
 
-getNonRepeatingNumber.a = ['01', '02', '03', '04', '05', '06', '07', '08'];
+  for (var i = 0; i < featureCount; i++) {
+    var randomFeature = allFeatures.splice(getRandomNumber(0, allFeatures.length));
+    features.push(randomFeature);
+  }
+
+  return features;
+}
+
+// генерим рандомный адрес
+function getNewAddr() {
+  var x = getRandomNumber(300, 900);
+  var y = getRandomNumber(100, 500);
+  return {'x': x, 'y': y};
+}
 
 // генерим неповторяющиеся заголовки
 var allTitle = [
@@ -28,60 +49,17 @@ var allTitle = [
   'Уютное бунгало далеко от моря',
   'Неуютное бунгало по колено в воде',
 ];
+
 function createAd(index) {
-  var ad = {
-      author: {
-        avatar: 'img/avatar/user' + getNonRepeatingNumber(index + 1) + '.png',
-      },
-      features: getRandomFeatures(),
-      title: allTitle[index],
-    };
-    return ad;
-  };
-
-  for (var i = 0; i < 8; i++) {
-  createAd(i);
-}
-
-// генерим рандомный адрес
-function getNewAddr() {
-  var x = getRandomNumber(300, 900);
-  var y = getRandomNumber(100, 500);
-  return { 'x': x, 'y': y };
-}
-
-// рандомные удобства
-function getRandomFeatures() {
-  var features = [];
-  var feats = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner',
-  ];
-  var featureCount = getRandomNumber(0, feats.length);
-
-  for (var i = 0; i < featureCount; i++) {
-    var randomFeature = feats.splice(getRandomNumber(0, feats.length), 1);
-    features.push(randomFeature);
-  }
-
-  return features;
-}
-
-// генерим итоговый объект
-function getNewObj() {
   var addr = getNewAddr();
-  return {
+  var ad = {
     'author': {
-      'avatar': 'img/avatars/user' + getNonRepeatingNumber() + '.png',
+      'avatar': 'img/avatar/user0' + (index + 1) + '.png',
     },
     'offer': {
-      'title': getNonRepeatingTitle(),
+      'title': allTitle[index],
       'price': getRandomNumber(1000, 1000000),
-      'address': addr.x + ', ' + addr.y,
+      'address': addr,
       'type': randItem(['flat', 'house', 'bungalo']),
       'rooms': getRandomNumber(1000, 1000000),
       'guests': getRandomNumber(1, 5),
@@ -96,69 +74,35 @@ function getNewObj() {
       'y': addr.y
     },
   };
-};
+  return ad;
+}
 
-var objectList = [];
+var ads = [];
 for (var i = 0; i < 8; i++) {
-  obj = getNewObj();
-  objectList.push(obj);
+  var ad = createAd(i);
+  ads.push(ad);
 }
 
 // создаем DOM-элементы
-var divPin;
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < objectsList.length; i++) {
-  var newElement = document.createElement('div');
-  divPin.className = 'pin';
-  divPin.style.left = objectsList[i].addr.x - pin.offsetWidth / 2  + 'px';
-  divPin.style.top = objectsList[i].addr.y - pin.offsetHeight / 2  + 'px';
-  divPin.innerHTML = '<img src=\"img/avatars/user" + getNonRepeatingNumber() + ".png\" class=\"rounded\" width=\"40\" height=\"40\">';
+for (var y = 0; y < ads.length; y++) {
+  var pin = document.createElement('div');
+  pin.classList.add = 'pin';
+  pin.style.left = ads[i].addr - pin.offsetWidth / 2 + 'px';
+  pin.style.top = ads[i].addr - pin.offsetHeight / 2 + 'px';
+  pin.innerHTML = '<img src="img/avatars/user0' + ad.author.avatar + '.png" class="rounded" width="40" height="40">';
 }
 
-  fragment.appendChild(divPin);
+fragment.appendChild(pin);
 
-
-//Добавление пинов в DOM
-var pinsElem = function (divPin) {
-var mapsLoc = document.querySelector('.tokyo__pin-map');
-var objectLoc = document.createDocumentFragment();
-  for (var i = 0; i < divPin.length; i++) {
-    objectLoc.appendChild(createPin(divPin[i]));
+// Добавление пинов в DOM
+var pinAd = function (ad) {
+  var map = document.querySelector('.tokyo__pin-map');
+  var adLoc = document.createDocumentFragment();
+  for (var i = 0; i < ad.length; i++) {
+    adLoc.appendChild(pinAd);
   }
 
-  return mapsLoc.appendChild(objectLoc);
+  return map.appendChild(ad);
 };
-
-// заполненение шаблона
-var objectTemplate = function (template, object) {
-
-  var avatar = document.querySelector('.dialog__title > img');
-  avatar.src = object.author.avatar;
-
-  var similarObjectsTemplate = document.getElementById('lodge-template').content.cloneNode(true);
-  var offerTitle = document.querySelector('.lodge__title').textContent = object.offer.title;
-  var offerAddress = document.querySelector('.lodge__address').textContent = object.offer.address;
-  var offerPrice = document.querySelector('.lodge__price').textContent = object.offer.price + ' &#8381;/ночь';
-  var offerType = document.querySelector('.lodge__type').textContent = object.offer.type;
-  var offerRoomsGuests = document.querySelector('.lodge__rooms-and-guests').textContent = object.offer.rooms + ' комнаты для ' + object.offer.guests + ' гостей';
-  var offerCheckinTime = document.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + object.offer.checkin + ',' + ' выезд до ' + object.offer.checkout;
-  var offerFeatures = document.querySelector('.lodge__features')
-  var offerDescription = document.querySelector('.lodge__description').textContent = object.offer.description;
-
-  var featuresFragment = getRandomFeatures(object.offer.features);
-  var lodgeFeatures = document.createElement('div');
-  lodgeFeatures.classList.add('lodge__features');
-  lodgeFeatures.appendChild(featuresFragment);
-
-  for (var i = 0; i < object.offer.features.length; i++) {
-    var objectFeature = document.createElement('span');
-    objectFeature.className = 'feature__image  feature__image--' + object.offer.features[i];
-    document.querySelector('.lodge__features').appendChild(span);
-  }
-
-  return objectTemplate;
-};
-
-var dialogPanel = document.querySelector('.dialog_panel');
-dialogPanel.replaceChild(objectTemplate, dialogPanel);
